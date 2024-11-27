bbdfd597c5868775d23dbcaf12ceced411bcc171
Janitha S. Ranasinghe <janitha479@gmail.com>
2024-11-28 02:13:44 +0530

    changed the navbar

--- src/app/Pagers/student-create/student-create.component.ts
+++ src/app/Pagers/student-create/student-create.component.ts
@@ -1,58 +1,50 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; // Import Router
 import { StudentService } from 'src/app/Service/student.service';
 
 @Component({
   selector: 'app-student-create',
   templateUrl: './student-create.component.html',
-  styleUrls: ['./student-create.component.css']
+  styleUrls: ['./student-create.component.css'],
 })
-
-
-
 export class StudentCreateComponent {
+  constructor(
+    private studentService: StudentService,
+    private router: Router // Inject Router
+  ) {}
 
-  constructor(private studentService: StudentService){}
+  name!: string;
+  nic!: string;
+  course!: string;
+  contact!: string;
+  isLoading: boolean = false;
+  errors: any = [];
 
-name !: string
-nic !: string
-course !: string
-contact !: string
-isLoading: boolean = false
-errors: any =[];
+  saveStd() {
+    this.isLoading = true;
 
-saveStd(){
-  this.isLoading = true
+    const inputData = {
+      name: this.name, // API function variable names in Laravel
+      nic: this.nic,
+      course: this.course,
+      contact: this.contact,
+    };
 
-  var inputData = {
-    name: this.name,//these are the variable names of api function variable names in laravel ($request->name;)
-    nic: this.nic,
-    course: this.course,
-    contact: this.contact,
+    this.studentService.saveStd(inputData).subscribe({
+      next: (res: any) => {
+        console.log(res, 'response');
+        alert('Student Added Successfully');
+        this.isLoading = false;
 
+        // Redirect to /student after success
+        this.router.navigate(['/student']);
+      },
+      error: (err: any) => {
+        this.errors = err.error.errors;
+        this.isLoading = false;
 
+        console.log(err.error.errors, 'errors');
+      },
+    });
   }
-  this.studentService.saveStd(inputData).subscribe({
-    next: (res: any) => {
-      console.log(res, 'response');
-      alert(res.message);
-      this.name = '';
-      this.nic = '';
-      this.course = '';
-      this.contact = '';
-      this.isLoading = false
-
-    },
-    error: (err: any) => {
-      this.errors = err.error.errors
-      this.isLoading = false
-
-      console.log(err.error.errors, 'errors');
-
-
-    }
-
-  })
-
-}
-
 }
